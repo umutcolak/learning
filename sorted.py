7c4bd7e5eb30c31d996b01e15f2ace943405d277
umutcolak <umut.yasin.colak@hotmail.com>
2023-06-19 02:19:04 +0300

    Example

--- sorted.py
+++ sorted.py
@@ -1,13 +1,14 @@
 # bubble Sort yöntemi
-def sort(my_list):
+my_list = ['T', 'P', 'A', 'N', 'M', 'G', 'S', 'R', 'V', 'H', 'Q', 'C', 'U', 'Y', 'Z', 'D', 'B', 'K', 'J', 'X', 'O', 'I',
+           'W', 'L', 'F', 'E']
+
+
+def sort_1(my_list):
     for index in range(len(my_list) - 1):
         for index_2 in range(len(my_list) - 1 - index):
             if my_list[index_2] > my_list[index_2 + 1]:
                 my_list[index_2], my_list[index_2 + 1] = my_list[index_2 + 1], my_list[index_2]
 
 
-my_list = ['T', 'P', 'A', 'N', 'M', 'G', 'S', 'R', 'V', 'H', 'Q', 'C', 'U', 'Y', 'Z', 'D', 'B', 'K', 'J', 'X', 'O', 'I',
-           'W', 'L', 'F', 'E']
-
-sort(my_list)
+# sort_1(my_list)
 print(my_list)
